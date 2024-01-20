a4a31a82139e9ef1fe9abd69fe8fabd678a22929
shashoto-nur <shashoto.nur@proton.me>
2024-01-20 18:27:56 +0600

    Updates

--- assets/exportMarkdown-FI0QSQ96.js
+++
@@ -1,2 +0,0 @@
-import{s}from"./index-G1RmKFi7.js";const a=({filename:o,markdown:t})=>{const e=new Blob([t],{type:"text/markdown"});s(e,`${o}.md`)};export{a as e};
-//# sourceMappingURL=exportMarkdown-FI0QSQ96.js.map
---
+++ assets/exportMarkdown-prvLWrxa.js
@@ -0,0 +1,2 @@
+import{s}from"./index-cG9zMz_t.js";const a=({filename:o,markdown:t})=>{const e=new Blob([t],{type:"text/markdown"});s(e,`${o}.md`)};export{a as e};
+//# sourceMappingURL=exportMarkdown-prvLWrxa.js.map
