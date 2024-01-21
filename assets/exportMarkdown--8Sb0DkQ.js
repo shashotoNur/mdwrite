b9c8eb1afa506820c0bccb1b45dd780cfeafb0c3
shashoto-nur <shashoto.nur@proton.me>
2024-01-21 12:14:45 +0600

    Updates

---
+++ assets/exportMarkdown--8Sb0DkQ.js
@@ -0,0 +1,2 @@
+import{s}from"./index-L9p5gOat.js";const a=({filename:o,markdown:t})=>{const e=new Blob([t],{type:"text/markdown"});s(e,`${o}.md`)};export{a as e};
+//# sourceMappingURL=exportMarkdown--8Sb0DkQ.js.map
